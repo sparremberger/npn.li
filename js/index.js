e576df33ee1b748eaae5cec8753e3ac2ed42f07e
Alan Sparremberger <alan.rs@outlook.com>
2021-06-04 10:39:32 -0300

    mobile css

--- js/index.js
+++ js/index.js
@@ -9,17 +9,13 @@ function encurtar() {
 }
 
 function showLinks() {
-    const x = document.querySelector("#header-links > ul:nth-child(1)");
-    const y = document.querySelector("#header-links > ul:nth-child(2)");
-    
+    const x = document.querySelector("#header-links");
+
     if (x.style.display === "block") {
-      x.style.display = "none";
-      y.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "block";
-      y.style.display = "block";
-      console.log("blocou");
+        x.style.display = "block";
+
+        console.log("blocou");
     }
-  } 
-
-
+}
