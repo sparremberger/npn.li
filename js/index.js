4ef38a5c8a94a0dbbb87584eba85f36523001537
Alan Sparremberger <alan.rs@outlook.com>
2021-06-04 03:44:46 -0300

    refazendo o CSS seguindo o paradigma mobile-first

--- js/index.js
+++ js/index.js
@@ -8,4 +8,18 @@ function encurtar() {
         .catch((err) => console.error(err));
 }
 
+function showLinks() {
+    const x = document.querySelector("#header-links > ul:nth-child(1)");
+    const y = document.querySelector("#header-links > ul:nth-child(2)");
+    
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      y.style.display = "none";
+    } else {
+      x.style.display = "block";
+      y.style.display = "block";
+      console.log("blocou");
+    }
+  } 
+
 
